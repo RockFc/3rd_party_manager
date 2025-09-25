b44459b0c518fab79328831e699b197c9af517a2
RockFc <769357879@qq.com>
2025-09-25 10:53:02 +0800

    update

--- update_librdkafka.sh
+++ update_librdkafka.sh
@@ -5,17 +5,27 @@ SOURCE_PATH=./src
 mkdir -p ${SOURCE_PATH}
 cd ${SOURCE_PATH}
 if [ ! -e "librdkafka" ]; then
-    git clone --depth 1 https://github.com/confluentinc/librdkafka.git
+    git clone https://github.com/confluentinc/librdkafka.git
 fi
 cd librdkafka
-git checkout v2.8.0
+git checkout v2.4.0
 
 # compile librdkafka
 INSTALL_PATH=../../../out/librdkafka
 rm -rf build
 mkdir -p build
 cd build
-cmake -D CMAKE_INSTALL_PREFIX=${INSTALL_PATH} ..
+cmake   -D CMAKE_INSTALL_PREFIX=${INSTALL_PATH} \
+        -D RDKAFKA_BUILD_STATIC=ON \
+        -D RDKAFKA_BUILD_EXAMPLES=OFF \
+        -D RDKAFKA_BUILD_TESTS=OFF \
+        -D WITH_SSL=OFF \
+        -D WITH_SASL=OFF \
+        -D WITH_ZLIB=OFF \
+        -D WITH_CURL=OFF \
+        -D WITH_ZSTD=OFF \
+        -D ENABLE_LZ4_EXT=OFF \
+        ..
 cmake --build . -j20 --target install
 
 cd ../../..
--- update_src_pro.sh
+++ update_src_pro.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# iguana
+# iguana + frozen
 SOURCE_PATH=./src
 mkdir -p ${SOURCE_PATH}
 cd ${SOURCE_PATH}
 if [ ! -e "iguana" ]; then
-    git clone --depth 1 https://github.com/qicosmos/iguana.git
+    git clone https://github.com/qicosmos/iguana.git
 fi
 cd iguana
 git checkout 1.0.9
@@ -13,39 +13,41 @@ git checkout 1.0.9
 INSTALL_PATH=../../out
 mkdir -p ${INSTALL_PATH}
 cp -r iguana ${INSTALL_PATH}
+cp -r frozen ${INSTALL_PATH}
+cd ../..
+pwd
+
+# modern-cpp-kafka
+SOURCE_PATH=./src
+mkdir -p ${SOURCE_PATH}
+cd ${SOURCE_PATH}
+if [ ! -e "modern-cpp-kafka" ]; then
+    git clone https://github.com/morganstanley/modern-cpp-kafka.git
+fi
+cd modern-cpp-kafka
+git checkout v2024.07.03
+
+INSTALL_PATH=../../out
+mkdir -p ${INSTALL_PATH}
+cp -r include/kafka ${INSTALL_PATH}
 
 cd ../..
 pwd
 
-
+# prometheus-cpp-lite
 SOURCE_PATH=./src
 mkdir -p ${SOURCE_PATH}
 cd ${SOURCE_PATH}
-if [ ! -e "iguana" ]; then
-    git clone --depth 1 https://github.com/qicosmos/iguana.git
+if [ ! -e "prometheus-cpp-lite" ]; then
+    git clone https://github.com/biaks/prometheus-cpp-lite.git
 fi
-cd iguana
-git checkout 1.0.9
+cd prometheus-cpp-lite
+git checkout v1.0
 
-INSTALL_PATH=../../out
+INSTALL_PATH=../../out/prometheus-cpp-lite
 mkdir -p ${INSTALL_PATH}
-cp -r iguana ${INSTALL_PATH}
 
-cd ../..
-pwd
-
-SOURCE_PATH=./src
-mkdir -p ${SOURCE_PATH}
-cd ${SOURCE_PATH}
-if [ ! -e "iguana" ]; then
-    git clone --depth 1 https://github.com/qicosmos/iguana.git
-fi
-cd iguana
-git checkout 1.0.9
-
-INSTALL_PATH=../../out
-mkdir -p ${INSTALL_PATH}
-cp -r iguana ${INSTALL_PATH}
+cp -r * ${INSTALL_PATH}
 
 cd ../..
 pwd
